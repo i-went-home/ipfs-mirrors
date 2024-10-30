9026275729380e211dbc659e2b0c1e050b840529
i-went-home <165395673+i-went-home@users.noreply.github.com>
2024-10-31 02:52:24 +0300

    В GENTOO_MIRRORS указан путь до distfiles, а не distfiles

--- portage/ipfs-publish-portage-distfiles.sh
+++ portage/ipfs-publish-portage-distfiles.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
-IPFS_KEY="portage-distfiles"
-IPFS_DIR="/${IPFS_KEY}"
-DIR=~/${IPFS_KEY}/
+# В GENTOO_MIRRORS указан путь до distfiles, а не distfiles.
+IPFS_PUB_KEY="portage"
+IPFS_PUB_DIR="/${IPFS_PUB_KEY}"
+
+IPFS_DIR="${IPFS_PUB_DIR}/distfiles"
+DIR=~/${IPFS_DIR}/
 
 
 echo "files: find new"
 found=$(ipfs files ls ${IPFS_DIR})
-files=$(find ${DIR} -maxdepth 1 -type f \
- -not \( -path '*/[@.]*' -o -iname '*.__download__*' -o -iname '*fail*' \) \
- $(printf " -not -name %s " ${found}))
+if [[ -z "${found}" ]]
+then
+    files=$(find ${DIR} -maxdepth 1 -type f \
+        -not \( -path '*/[@.]*' -o -iname '*.__download__*' -o -iname '*fail*' \))
+else
+    files=$(find ${DIR} -maxdepth 1 -type f \
+        -not \( -path '*/[@.]*' -o -iname '*.__download__*' -o -iname '*fail*' \) \
+        $(printf " -not -name %s " ${found}))
+fi
+
 
 if [[ -z "${files}" ]]
 then
@@ -18,21 +28,21 @@ else
     # TODO: progress
     for f in ${files}
     do
-        ipfs add ${f} --progress=true --pin=false --nocopy=true --to-files ${IPFS_DIR}/
+        ipfs add ${f} --progress=true --pin=false --to-files ${IPFS_DIR}/
     done
 
     # publish ipns
-    key=$(ipfs key list | grep ${IPFS_KEY})
+    key=$(ipfs key list | grep ${IPFS_PUB_KEY})
     if [[ -z "${key}" ]]
     then
-        #ipfs key gen ${IPFS_KEY}
+        #ipfs key gen ${IPFS_PUB_KEY}
         echo "ipfs: no key"
     else
-        hash=$(ipfs files stat --hash ${IPFS_DIR})
+        hash=$(ipfs files stat --hash ${IPFS_PUB_DIR})
         if [[ ! -z "${hash}" ]]
         then
             echo "ipfs: publish"
-            ipfs name publish --key=${IPFS_KEY} /ipfs/${hash}
+            ipfs name publish --key=${IPFS_PUB_KEY} /ipfs/${hash}
         fi
     fi
 fi
